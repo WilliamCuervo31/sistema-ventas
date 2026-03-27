2d7bd25dcf360396d48a97694e76255846fd96ec
Natalia Trujillo <nataliatrujillocangrejo@gmail.com>
2026-03-26 22:09:12 -0500

    Se agregan logs en db_connection

--- config/db_connection.py
+++ config/db_connection.py
@@ -15,6 +15,7 @@ DB_PORT = os.getenv("DB_PORT")
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
+logger = logging.getLogger(__name__)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -25,8 +26,13 @@ SessionLocal = sessionmaker(
 Base = declarative_base()
 
 def get_db():
+    logger.info("Intentando conectar a la base de datos")
     db = SessionLocal()
     try:
+        logger.info("Conexión a la base de datos exitosa")
         yield db
+    except Exception as e:
+        logger.error(f"Error en la conexión: {e}")
     finally:
+        logger.info("Cerrando conexión a la base de datos")
         db.close()
